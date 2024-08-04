2339a7f58f6f8a2da57503686e74374b20e2f776
Harsh Goyal <harshgoyal0219@gmail.com>
2024-08-04 08:44:20 +0530

    first commit

---
+++ client/src/utils/constant.js
@@ -0,0 +1,9 @@
+
+const baseurl = import.meta.env.VITE_BASE_URL;
+
+
+
+export const USER_API_END_POINT =  `${baseurl}/user`;
+export const JOB_API_END_POINT =   `${baseurl}/job`;
+export const APPLICATION_API_END_POINT =  `${baseurl}/application`;
+export const COMPANY_API_END_POINT =    `${baseurl}/company`;
